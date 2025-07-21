4538d4ec67ebfdb166394070d9d0650e0a175142
Marat <kitvideo-pc3@ya.ru>
2025-07-21 07:44:44 +0300

    очень вяло

--- couriers/dialogs.py
+++ couriers/dialogs.py
@@ -1,6 +1,6 @@
 from aiogram_dialog import Dialog, Window
-from aiogram_dialog.widgets.kbd import Cancel, Select, Start, SwitchTo
-from aiogram_dialog.widgets.text import Const, Format
+from aiogram_dialog.widgets.kbd import Start, SwitchTo
+from aiogram_dialog.widgets.text import Const
 from couriers.states import MainSG, OrdersGS
 from orders.states import OrderSG
 
@@ -14,22 +14,5 @@ main_dialog = Dialog(
             state=MainSG.shift_calc,
         ),
         state=MainSG.main,
-    ),
-)
-
-orders = Dialog(
-    Window(
-        Select(
-            Format("item[address], item[price]₽, item[payment]"),
-            id="s_orders",
-            item_id_getter=lambda x: x["id"],
-            items="orders",
-        ),
-        Cancel(Const("Назад")),
-        SwitchTo(Const("Архив"), id="to_archive", state=OrdersGS.calendar),
-        state=OrdersGS.main,
-    ),
-    Window(
-        state=OrdersGS.calendar,
     ),
 )
--- orders/dialogs.py
+++ orders/dialogs.py
@@ -1,12 +1,28 @@
 from aiogram_dialog import Dialog, Window
+from aiogram_dialog.widgets.kbd import Cancel, Select, SwitchTo
+from aiogram_dialog.widgets.text import Const, Format
+from orders.states import OrderSG, OrdersSG
 
-from orders.states import NewOrderSG
-
-
+orders = Dialog(
+    Window(
+        Select(
+            Format("item[address], item[price]₽, item[payment]"),
+            id="s_orders",
+            item_id_getter=lambda x: x["id"],
+            items="orders",
+        ),
+        Cancel(Const("Назад")),
+        SwitchTo(Const("Архив"), id="to_archive", state=OrdersSG.calendar),
+        state=OrdersSG.main,
+    ),
+    Window(
+        state=OrdersSG.calendar,
+    ),
+)
 order = Dialog(
-    Window(state=NewOrderSG.location),
-    Window(state=NewOrderSG.address),
-    Window(state=NewOrderSG.price),
-    Window(state=NewOrderSG.payment),
-    Window(state=NewOrderSG.preview),
+    Window(state=OrderSG.location),
+    Window(state=OrderSG.address),
+    Window(state=OrderSG.price),
+    Window(state=OrderSG.payment),
+    Window(state=OrderSG.preview),
 )
