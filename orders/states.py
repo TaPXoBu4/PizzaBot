e6fa09d948c30a209a51692f19da3384b69b8b9d
Marat <kitvideo-pc3@ya.ru>
2025-07-21 07:44:44 +0300

    очень вяло

--- orders/states.py
+++ orders/states.py
@@ -1,6 +1,11 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
+class OrdersSG(StatesGroup):
+    main = State()
+    calendar = State()
+
+
 class OrderSG(StatesGroup):
     location = State()
     address = State()
